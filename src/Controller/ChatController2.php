469994ecbf8d0be4c2cb6c6fa7c2e7f3bba334c3
HaoualaMedAziz <azizhaouala6@gmail.com.com>
2025-05-19 12:08:34 +0100

    first commit

---
+++ src/Controller/ChatController2.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ChatController2 extends AbstractController
+{
+    #[Route('/chat2', name: 'chat_index2')]
+    public function index()
+    {
+        return $this->render('chat/index2.html.twig');
+    }
+
+    #[Route('/chat/send', name: 'chat_send', methods: ['POST'])]
+    public function send(Request $request): JsonResponse
+    {
+        $message = strtolower(trim($request->request->get('message')));
+
+        $reponses = [
+            'bonjour' => 'Bonjour ! Comment puis-je vous aider ? 😊',
+            'qui es-tu' => 'Je suis un chatbot simple développé avec Symfony.',
+            'aide' => 'Voici quelques commandes : bonjour, qui es-tu, merci...',
+            'merci' => 'Avec plaisir ! 😊',
+            'au revoir' => 'À bientôt 👋',
+            'who is gay in sousse?' => 'Ahmed Amara',
+            'are you sure?' => 'yes',
+            'who else?'=> 'Aymen amara',
+            'ok, thank you'=>'asba lik',
+
+        ];
+
+        $reply = 'Désolé, je ne comprends pas. 😅  essaye avec un autre message';
+
+        foreach ($reponses as $cle => $reponse) {
+            if (str_contains($message, $cle)) {
+                $reply = $reponse;
+                break;
+            }
+        }
+
+        return new JsonResponse(['reply' => $reply]);
+    }
+}
